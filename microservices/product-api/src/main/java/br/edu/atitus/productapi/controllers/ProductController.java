624ce85392c443aaaeafeb82bfd0e6905fde5746
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2026-09-11 21:08:49 -0300

    Melhorias product-api

--- microservices/product-api/src/main/java/br/edu/atitus/productapi/controllers/ProductController.java
+++ microservices/product-api/src/main/java/br/edu/atitus/productapi/controllers/ProductController.java
@@ -2,6 +2,11 @@ package br.edu.atitus.productapi.controllers;
 
 import br.edu.atitus.productapi.dtos.ProductResponse;
 import br.edu.atitus.productapi.services.ProductService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,10 +19,30 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public ProductResponse getById(
+    public ResponseEntity<ProductResponse> getById(
             @PathVariable Long id,
             @RequestParam String targetCurrency
     ) throws Exception {
-        return service.findById(id, targetCurrency);
+        var response = service.findById(id, targetCurrency);
+        return ResponseEntity.ok(response);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<Page<ProductResponse>> getAll(
+            @RequestParam String targetCurrency,
+            @PageableDefault(size = 10, sort = "description",page = 0)Pageable pageable
+            ) throws Exception {
+        var response = service.findAll(pageable, targetCurrency);
+        return ResponseEntity.ok(response);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> entityNotFoundExceptionHandler(EntityNotFoundException ex){
+        return ResponseEntity.status(404).body(ex.getMessage());
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exceptionHandler(Exception ex){
+        return ResponseEntity.status(500).body("Ops!!! Algo deu errado.");
     }
 }
